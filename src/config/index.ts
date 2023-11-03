cc5f1fcc1c55907effbd649743137b6fcf982a2b
esthvega <esthvega@hotmail.es>
2023-11-03 20:24:59 +0100

    feat: basic mongo config and insert subsequence

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,3 +3,7 @@ import 'dotenv/config';
 export const EXPIRATION_MILLISECONDS = process.env.EXPIRATION_MILLISECONDS ? parseInt(process.env.EXPIRATION_MILLISECONDS, 10) : 600 // 10 min
 
 export const SECRET_KEY = process.env.SECRET_KEY || '';
+
+export const MONGO_URL = process.env.MONGO_URL || 'mongodb://localhost:27017';
+
+export const DB_NAME = process.env.DB_NAME || 'sequences-db';
